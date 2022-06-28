daa067bbaf464b29944af6841876f582e304dd44
arxont <inknw1999@mail.ru>
2022-06-29 00:47:24 +0300

    New code

--- trials.js
+++ trials.js
@@ -67,3 +67,49 @@ function isTriangle(a, b, c)
 }
 
 /* console.log(isTriangle(0, 3, 2)) */
+
+
+/* Подсчет гласных букв в строке (за искл. 'y') */
+
+function myGetCount(str) {
+    const arrayStr = str.split('');
+    let count = 0;
+    arrayStr.forEach((item) => {
+        if (item === 'a' || item === 'e' || item === 'i' || item === 'o' || item === 'u') {
+            count += 1;
+        }
+    })
+
+    return count
+}
+
+/* более простой вариант */
+
+function getCount(str) {
+    return (str.match(/[aeiou]/ig)||[]).length;
+  }
+
+/* console.log(getCount('genuya')) */
+
+
+/* Сумма двух самых маленьких положительных целых чисел в массиве */
+
+function sumTwoSmallestNumbers(numbers) {
+    numbers.sort((a, b) => a - b);
+    return numbers[0] + numbers[1];
+}
+
+/* console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43])) */
+
+
+/* Валидация ПИН-кода состоящего из 4-х или 6-и цифр */
+
+function validatePIN(pin) {
+    const regex = /^(\d{4}|\d{6})$/;
+    return regex.test(pin);
+}
+
+/* console.log(validatePIN('1234f'));
+console.log(validatePIN('1234'));
+console.log(validatePIN('12345'));
+console.log(validatePIN('123456')); */
